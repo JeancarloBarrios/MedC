8392abb40fcbd2e188730a34ba8fde0d0b4ae4a0
Jeancarlo Barrios <jeancarlobarrios@gmail.com>
2017-11-20 15:36:06 -0600

    added rest framework

--- medc/medc/settings.py
+++ medc/medc/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Python packages
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
